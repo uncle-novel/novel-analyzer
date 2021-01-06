e216b71ad8d475152539c548d0ad842ad9db2d20
unclezs <1585503310@qq.com>
2021-01-06 13:59:32 +0800

    :sparkles: phantomjs支持

--- script/spider.js
+++ script/spider.js
@@ -2,6 +2,7 @@
  * phantomjs 解析动态网页
  * 支持传入referer、cookie、useragent
  *
+ * 注意：不能使用let声明变量 使用参照 com.unclezs.novel.core.request.PhantomJsClientTest
  * @author blog.unclezs.com
  * @date   2020-12-24
  * @see    https://phantomjs.org/api/
